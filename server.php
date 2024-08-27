7de49df0c8a42137960aaeb45bb8a433f5de260f
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-08-27 17:05:54 +0200

    feat: add Api from Json

--- server.php
+++ server.php
@@ -1,2 +1,17 @@
 <?php
 
+// leggo il json e lo salvo in stringa
+$json_string = file_get_contents('todo-list.json');
+
+// converto stringa in elemento php (true - stringa => array associativo)
+$list = json_decode($json_string, true);
+
+
+// codice php
+
+
+// leggo il file php come fosse in Json
+header('Content-Type: application/json');
+
+// ritorno la lista in json e la stampo
+echo json_encode($list);
